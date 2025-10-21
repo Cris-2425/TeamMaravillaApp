dfb0b0803d9d606b9697c266c1b9990de0ff566f
Cristian Celso Iglesias Rodríguez <a19cristiancir@teis.intranet>
2025-10-21 17:24:44 +0200

    Cambios clase BottomBar

--- app/src/main/java/com/example/teammaravillaapp/BottomBar.kt
+++ app/src/main/java/com/example/teammaravillaapp/BottomBar.kt
@@ -1,7 +1,28 @@
 package com.example.teammaravillaapp
 
-fun BottomBar(
-    
-) {
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
 
+enum class BotonOpciones { INICIO, PERFIL, CAMARA, RECETAS, SALIR }
+
+@Composable
+fun BottomBar(
+    botonSeleccionado: BotonOpciones,
+    botonInicio: () -> Unit = {},
+    botonPerfil: () -> Unit = {},
+    botonCamara: () -> Unit = {},
+    botonRecetas: () -> Unit = {},
+    botonSalir: () -> Unit = {}
+) {
+    NavigationBar {
+        NavigationBarItem(
+            selected = BotonOpciones.INICIO,
+            onClick = {
+                Log.e("Barra inferior", "Ir a inicio."),
+            icon = 
+            }
+        )
+    }
+    }
 }
