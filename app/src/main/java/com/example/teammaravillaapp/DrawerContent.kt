fad125d6f28014b1b79aaf2faa78f6c8c2f43004
Cristian Celso Iglesias Rodríguez <a19cristiancir@teis.intranet>
2025-10-23 19:45:33 +0200

    Añadiendo cosas

---
+++ app/src/main/java/com/example/teammaravillaapp/DrawerContent.kt
@@ -0,0 +1,54 @@
+package com.example.teammaravillaapp
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun DrawerContent() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    Spacer(Modifier.height(12.dp))
+    Text(
+        text = "Notificaciones",
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                // Handle click
+                scope.launch { drawerState.close() }
+            }
+            .padding(16.dp)
+    )
+    Text(
+        text = "Compartir lista",
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                // Handle click
+                scope.launch { drawerState.close() }
+            }
+            .padding(16.dp)
+    )
+    Text(
+        text = "Opciones",
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                // Handle click
+                scope.launch { drawerState.close() }
+            }
+            .padding(16.dp)
+    )
+}
